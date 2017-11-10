71b40d63e628dbc40f71e0fabc61ef74b9434c54
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-11-10 11:31:54 +0100

    project: structure rearangement

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 mkdir -p build
 
-g++ -std=c++17 -c main.cxx -o build/main.o -fno-builtin -nostdlib -m32 -fno-rtti -fno-exceptions -fno-pie -fno-pic -ffreestanding
-g++ -std=c++17 -c init_globals.cxx -o build/init_globals.o -fno-builtin -nostdlib -m32 -fno-rtti -fno-exceptions -fno-pie -fno-pic -ffreestanding
-nasm -f elf32 start.asm -o build/start.o
+CXX_FLAGS="-std=c++17 -fno-builtin -nostdlib -m32 -fno-rtti -fno-exceptions -fno-pie -fno-pic -ffreestanding"
+INCLUDES="-Ikernel"
+
+g++ $CXX_FLAGS $INCLUDES -c kernel/main.cxx -o build/main.o
+g++ $CXX_FLAGS $INCLUDES -c kernel/init_globals.cxx -o build/init_globals.o
+nasm -f elf32 start/start.asm -o build/start.o
 
 ld build/main.o build/start.o build/init_globals.o -o build/kernel -melf_i386 -T link.ld
